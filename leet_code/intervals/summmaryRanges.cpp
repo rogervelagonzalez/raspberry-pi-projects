26e55ca4a63e3227f5987323f1f42223bf71bf1f
Roger Vela Gonzalez <rogervelagonzalez@gmail.com>
2026-01-05 18:39:24 +0100

    summaryRanges updated and working.

--- leet_code/intervals/summmaryRanges.cpp
+++ leet_code/intervals/summmaryRanges.cpp
@@ -6,36 +6,23 @@ public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> myList;
         string sResult;
-        if(nums.empty()){
-            return myList;
-        }else if(nums.size() == 1){
-            sResult = to_string(nums[0]);
-            myList.push_back(sResult);
+        int n = nums.size();
+        if (n == 0){
             return myList;
         }
-        int maxRange = 0;
-        
-        int left = 0, right = 0;
-        int candidateLeft = nums[left];
-        int candidateRight = nums[right];
-        while(right<nums.size()){
-            if(nums[right+1]-nums[left] == 1){
-                candidateRight = nums[right+1];
-                right++;
-                left++;
-            }else{
-                if(candidateLeft != candidateRight){
-                    sResult = to_string(candidateLeft) + "->" + to_string(candidateRight);
+        int startLeft = nums[0];
+
+        for (int i = 1; i <= n; i++) {
+            if(i==n || nums[i] != nums[i-1] + 1){
+                if(nums[i-1] == startLeft){
+                    sResult = to_string(startLeft);
                 }else{
-                    sResult = to_string(candidateLeft);
+                    sResult = to_string(startLeft) + "->" + to_string(nums[i-1]);
                 }
                 myList.push_back(sResult);
-                if(right < (int)nums.size()){
-                    right++;
+                if(i < n){
+                    startLeft = nums[i];
                 }
-                left++;
-                candidateLeft = nums[left];
-                candidateRight = nums[right];
             }
         }
         return myList;
